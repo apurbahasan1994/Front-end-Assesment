270c4691957c370566fb9762c0bcac0a0e6d5980
apurba <apurbahasan1994@gmail.com>
2022-08-27 18:28:59 +0600

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppointmentComponent } from './components/appointment/appointment.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {path:"",component:AppointmentComponent},
+  {path:"month/:id",component:AppointmentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
